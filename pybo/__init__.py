1f7234ae43ad9f89cbd4d62cc1cb48283998bfc0
youngmin109 <qhqo3325@gmail.com>
2025-07-14 23:24:25 +0900

    study: flask model, SQL

--- pybo/__init__.py
+++ pybo/__init__.py
@@ -8,7 +8,11 @@ import config
 # migrate는 데이터베이스 스키마 변경을 관리하는 도구
 db = SQLAlchemy()
 migrate = Migrate()
-
+# 이 두 객체는 app 없이도 미리 생성해두고, 
+# 나중에 init_app() 메서드를 호출하여 Flask 애플리케이션에 연결하는 방식
+# 플라스크에서는 이러한 패턴을 많이 씀
+# db, migrate 객체를 create_app()함수 안에다 생성하면,
+# 블루프린트와 같은 다른 모듈에서 불러올 수 없다.
 
 def create_app():
     app = Flask(__name__) # __name__은 현재 모듈의 이름을 나타냄
